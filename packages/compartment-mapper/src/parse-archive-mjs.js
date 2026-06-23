endojs/endo

perf(bundle-source): speed up multi-entry bundling and add profiling detail

packages/compartment-mapper/src/parse-archive-mjs.js
@@ -11,6 +11,10 @@ import { ModuleSource } from '@endo/module-source';
 
 const textEncoder = new TextEncoder();
 const textDecoder = new TextDecoder();
+/** @type {Map<string, Map<string, ReturnType<ParseFn>>>} */
+const parseArchiveMjsCache = new Map();
+const MAX_PARSE_ARCHIVE_MJS_CACHE_ENTRIES = 20_000;
+let parseArchiveMjsCacheEntries = 0;
 
 /** @type {ParseFn} */
 export const parseArchiveMjs = (
@@ -21,7 +25,29 @@ export const parseArchiveMjs = (
   options = {},
 ) => {
   const { sourceMap, sourceMapHook, profileStartSpan } = options;
+  const canUseCache = sourceMapHook === undefined;
   const source = textDecoder.decode(bytes);
+  const sourceMapKey =
+    sourceMap === undefined
+      ? ''
+      : typeof sourceMap === 'string'
+        ? sourceMap
+        : JSON.stringify(sourceMap);
+  const cacheKey = `${source}\n//# sourceMappingURL=${sourceMapKey}`;
+  if (canUseCache) {
+    const byLocation = parseArchiveMjsCache.get(sourceUrl);
+    const cached = byLocation?.get(cacheKey);
+    if (cached !== undefined) {
+      profileStartSpan?.('compartmentMapper.parseArchiveMjs.cache.hit')?.();
+      return cached;
+    }
+    profileStartSpan?.('compartmentMapper.parseArchiveMjs.cache.miss')?.();
+  } else {
+    profileStartSpan?.('compartmentMapper.parseArchiveMjs.cache.bypass')?.({
+      hasSourceMap: sourceMap !== undefined,
+      hasSourceMapHook: sourceMapHook !== undefined,
+    });
+  }
   const endModuleSource = profileStartSpan?.(
     'compartmentMapper.parseArchiveMjs.moduleSource',
   );
@@ -37,11 +63,29 @@ export const parseArchiveMjs = (
   );
   const pre = textEncoder.encode(JSON.stringify(record));
   endStringify?.();
-  return {
+  const result = {
     parser: 'pre-mjs-json',
     bytes: pre,
     record,
   };
+  if (canUseCache) {
+    let byLocation = parseArchiveMjsCache.get(sourceUrl);
+    if (byLocation === undefined) {
+      byLocation = new Map();
+      parseArchiveMjsCache.set(sourceUrl, byLocation);
+    }
+    if (!byLocation.has(cacheKey)) {
+      parseArchiveMjsCacheEntries += 1;
+      if (parseArchiveMjsCacheEntries > MAX_PARSE_ARCHIVE_MJS_CACHE_ENTRIES) {
+        parseArchiveMjsCache.clear();
+        parseArchiveMjsCacheEntries = 0;
+        byLocation = new Map();
+        parseArchiveMjsCache.set(sourceUrl, byLocation);
+      }
+    }
+    byLocation.set(cacheKey, result);
+  }
+  return result;
 };
 
 /** @type {import('./types.js').ParserImplementation} */

endojs/endo-but-for-bots

packages/compartment-mapper/src/parse-archive-mjs.js
@@ -11,6 +11,38 @@ import { ModuleSource } from '@endo/module-source';
 
 const textEncoder = new TextEncoder();
 const textDecoder = new TextDecoder();
+/** @type {Map<string, Map<string, Map<string | undefined, ReturnType<ParseFn>>>>} */
+const parseArchiveMjsCache = new Map();
+const MAX_PARSE_ARCHIVE_MJS_CACHE_ENTRIES = 20_000;
+let parseArchiveMjsCacheEntries = 0;
+
+/**
+ * @param {string | object | undefined} sourceMap
+ * @returns {string | undefined}
+ */
+const getSourceMapCacheKey = sourceMap => {
+  if (sourceMap === undefined) {
+    return undefined;
+  }
+  if (typeof sourceMap === 'string') {
+    return sourceMap;
+  }
+  return JSON.stringify(sourceMap);
+};
+
+const evictOldestParseArchiveMjsLocation = () => {
+  const oldest = parseArchiveMjsCache.entries().next().value;
+  if (oldest === undefined) {
+    return;
+  }
+  const [sourceUrl, bySource] = oldest;
+  let evictedEntries = 0;
+  for (const bySourceMap of bySource.values()) {
+    evictedEntries += bySourceMap.size;
+  }
+  parseArchiveMjsCache.delete(sourceUrl);
+  parseArchiveMjsCacheEntries -= evictedEntries;
+};
 
 /** @type {ParseFn} */
 export const parseArchiveMjs = (
@@ -21,7 +53,23 @@ export const parseArchiveMjs = (
   options = {},
 ) => {
   const { sourceMap, sourceMapHook, profileStartSpan } = options;
+  const canUseCache = sourceMapHook === undefined;
   const source = textDecoder.decode(bytes);
+  const sourceMapKey = getSourceMapCacheKey(sourceMap);
+  if (canUseCache) {
+    const byLocation = parseArchiveMjsCache.get(sourceUrl);
+    const cached = byLocation?.get(source)?.get(sourceMapKey);
+    if (cached !== undefined) {
+      profileStartSpan?.('compartmentMapper.parseArchiveMjs.cache.hit')?.();
+      return cached;
+    }
+    profileStartSpan?.('compartmentMapper.parseArchiveMjs.cache.miss')?.();
+  } else {
+    profileStartSpan?.('compartmentMapper.parseArchiveMjs.cache.bypass')?.({
+      hasSourceMap: sourceMap !== undefined,
+      hasSourceMapHook: sourceMapHook !== undefined,
+    });
+  }
   const endModuleSource = profileStartSpan?.(
     'compartmentMapper.parseArchiveMjs.moduleSource',
   );
@@ -37,11 +85,33 @@ export const parseArchiveMjs = (
   );
   const pre = textEncoder.encode(JSON.stringify(record));
   endStringify?.();
-  return {
+  const result = {
     parser: 'pre-mjs-json',
     bytes: pre,
     record,
   };
+  if (canUseCache) {
+    let byLocation = parseArchiveMjsCache.get(sourceUrl);
+    if (byLocation === undefined) {
+      byLocation = new Map();
+      parseArchiveMjsCache.set(sourceUrl, byLocation);
+    }
+    let bySource = byLocation.get(source);
+    if (bySource === undefined) {
+      bySource = new Map();
+      byLocation.set(source, bySource);
+    }
+    if (!bySource.has(sourceMapKey)) {
+      bySource.set(sourceMapKey, result);
+      parseArchiveMjsCacheEntries += 1;
+      if (parseArchiveMjsCacheEntries > MAX_PARSE_ARCHIVE_MJS_CACHE_ENTRIES) {
+        evictOldestParseArchiveMjsLocation();
+      }
+    } else {
+      bySource.set(sourceMapKey, result);
+    }
+  }
+  return result;
 };
 
 /** @type {import('./types.js').ParserImplementation} */