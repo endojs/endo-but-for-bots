endojs/endo

feat(profiling): add compartment-mapper phase spans

packages/compartment-mapper/src/types/external.ts
@@ -156,12 +156,26 @@ export interface LogOptions {
 }
 
 /**
+ * Options having an optional profiling span hook.
+ */
+export interface ProfilingOptions {
+  /**
+   * Starts a profiling span and returns an end function.
+   */
+  profileStartSpan?: (
+    name: string,
+    args?: Record<string, unknown>,
+  ) => (args?: Record<string, unknown>) => void;
+}
+
+/**
  * Options for `mapNodeModules()`
  */
 export type MapNodeModulesOptions = MapNodeModulesOptionsOmitPolicy &
   PolicyOption &
   MapNodeModulesHookOptions &
-  LogOptions;
+  LogOptions &
+  ProfilingOptions;
 
 type MapNodeModulesOptionsOmitPolicy = Partial<{
   /** @deprecated renamed `conditions` to be consistent with Node.js */
@@ -273,7 +287,8 @@ export type ArchiveLiteOptions = SyncOrAsyncArchiveOptions &
   ImportingOptions &
   ExitModuleImportHookOption &
   LinkingOptions &
-  LogOptions;
+  LogOptions &
+  ProfilingOptions;
 
 export type SyncArchiveLiteOptions = SyncOrAsyncArchiveOptions &
   SyncModuleTransformsOption &

endojs/endo-but-for-bots

packages/compartment-mapper/src/types/external.ts
@@ -193,12 +193,26 @@ export interface LogOptions {
 }
 
 /**
+ * Options having an optional profiling span hook.
+ */
+export interface ProfilingOptions {
+  /**
+   * Starts a profiling span and returns an end function.
+   */
+  profileStartSpan?: (
+    name: string,
+    args?: Record<string, unknown>,
+  ) => (endArgs?: Record<string, unknown>) => void;
+}
+
+/**
  * Options for `mapNodeModules()`
  */
 export type MapNodeModulesOptions = MapNodeModulesOptionsOmitPolicy &
   PolicyOption &
   MapNodeModulesHookOptions &
-  LogOptions;
+  LogOptions &
+  ProfilingOptions;
 
 type MapNodeModulesOptionsOmitPolicy = Partial<{
   /** @deprecated renamed `conditions` to be consistent with Node.js */
@@ -350,7 +364,8 @@ export type ArchiveLiteOptions = SyncOrAsyncArchiveOptions &
   ImportingOptions &
   ExitModuleImportHookOption &
   LinkingOptions &
-  LogOptions;
+  LogOptions &
+  ProfilingOptions;
 
 export type SyncArchiveLiteOptions = SyncOrAsyncArchiveOptions &
   SyncModuleTransformsOption &
@@ -801,7 +816,8 @@ export type ParseOptions<
   readPowers: ReadFn | ReadPowers | undefined;
   compartmentDescriptor: TCompartmentDescriptor | undefined;
 }> &
-  ArchiveOnlyOption;
+  ArchiveOnlyOption &
+  ProfilingOptions;
 
 /**
  * Arguments for a {@link ParseFn} or {@link AsyncParseFn}.