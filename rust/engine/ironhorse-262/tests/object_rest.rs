292c3d13607db7d792fab8984ecf01a55e5f75e6
kumavis <aaron@kumavis.me>
2026-09-03 16:59:53 +0900

    fix(ironhorse): copy exotic object spread sources

--- rust/engine/ironhorse-262/tests/object_rest.rs
+++ rust/engine/ironhorse-262/tests/object_rest.rs
@@ -45,3 +45,43 @@ fn primitive_string_and_symbol_sources_are_boxed() {
         "0",
     );
 }
+
+#[test]
+fn spread_and_rest_copy_exotic_sources_observably() {
+    assert_result_agrees(
+        "var copy={...['a','b']};Object.keys(copy).join('|')+':'+copy[0]+':'+copy[1]",
+        "0|1:a:b",
+    );
+    assert_result_agrees(
+        "var a=[];a.length=3;a[2]='c';var copy={...a};Object.keys(copy).join('|')+':'+copy[2]",
+        "2:c",
+    );
+    assert_result_agrees(
+        "var copy={...new Uint8Array([4,5])};copy[0]+':'+copy[1]+':'+Object.keys(copy).join('|')",
+        "4:5:0|1",
+    );
+    assert_result_agrees(
+        "var log=[];var target={a:1,b:2};var source=new Proxy(target,{ownKeys:function(t){log.push('own');return Reflect.ownKeys(t)},getOwnPropertyDescriptor:function(t,k){log.push('desc:'+String(k));return Reflect.getOwnPropertyDescriptor(t,k)},get:function(t,k,r){log.push('get:'+String(k));return Reflect.get(t,k,r)}});var copy={...source};copy.a+copy.b+':'+log.join(',')",
+        "3:own,desc:a,get:a,desc:b,get:b",
+    );
+    assert_result_agrees(
+        "var symbol=Symbol('s');var source={[symbol]:7};var copy={...source};copy[symbol]",
+        "7",
+    );
+    assert_result_agrees(
+        "var calls=0;var proto={set x(v){calls++}};var copy={__proto__:proto,...{x:3}};copy.x+':'+calls+':'+copy.hasOwnProperty('x')",
+        "3:0:true",
+    );
+    assert_result_agrees(
+        "var copy={...null,...undefined,...'ab',...3};Object.keys(copy).join('|')+':'+copy[0]+copy[1]",
+        "0|1:ab",
+    );
+    assert_result_agrees(
+        "var source={get a(){delete this.b;return 1},b:2};var copy={...source};copy.a+':'+copy.hasOwnProperty('b')",
+        "1:false",
+    );
+    assert_result_agrees(
+        "var {0:first,...rest}=['a','b'];first+':'+rest[1]+':'+Object.keys(rest).join('|')",
+        "a:b:1",
+    );
+}
