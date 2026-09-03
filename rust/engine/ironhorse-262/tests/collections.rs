956a2d920341c5c24a44fc428d5129bc24f3177a
kumavis <aaron@kumavis.me>
2026-09-03 15:34:11 +0900

    feat(ironhorse): consume general collection iterables

--- rust/engine/ironhorse-262/tests/collections.rs
+++ rust/engine/ironhorse-262/tests/collections.rs
@@ -62,6 +62,68 @@ fn dense_array_iterables_populate_all_collection_kinds() {
 }
 
 #[test]
+fn collection_constructors_consume_general_and_sparse_iterables() {
+    agrees("var s=new Set(function*(){yield 1;yield 2;yield 1}());s.size+':'+s.has(2)");
+    agrees(
+        "var m=new Map(function*(){yield ['a',1];yield ['b',2]}()); \
+         m.size+':'+m.get('b')",
+    );
+    agrees("var s=new Set([,1]);s.size+':'+s.has(undefined)");
+    agrees("new Map([[,2]]).get(undefined)");
+    agrees("var s=new Set('a\\u{1F600}a');s.size+':'+s.has('\\u{1F600}')");
+    agrees(
+        "var a={},b={};var w=new WeakMap(function*(){yield [a,1];yield [b,2]}()); \
+         w.get(a)+w.get(b)",
+    );
+}
+
+#[test]
+fn collection_constructor_observes_protocol_order_and_closing() {
+    agrees(
+        "var log=[];var old=Set.prototype.add;Set.prototype.add=function(v){log.push('add'+v);return old.call(this,v)}; \
+         var a=[1,2];a[Symbol.iterator]=function(){log.push('iter');return [3][Symbol.iterator]()}; \
+         var s=new Set(a);Set.prototype.add=old;log.join(',')+':'+s.has(3)",
+    );
+    agrees(
+        "var closed=0,i=0;var it={ [Symbol.iterator]:function(){return { \
+           next:function(){return i++?{done:true}:{value:1,done:false}}, \
+           return:function(){closed++;return {}}}}}; \
+         var ok=false;try{new Map(it)}catch(e){ok=e instanceof TypeError}ok+':'+closed",
+    );
+    agrees(
+        "var closed=0;var it={ [Symbol.iterator]:function(){return { \
+           next:function(){throw 7},return:function(){closed++;return {}}}}}; \
+         var caught;try{new Set(it)}catch(e){caught=e}caught+':'+closed",
+    );
+    agrees(
+        "var closed=0,i=0;var it={ [Symbol.iterator]:function(){return { \
+           next:function(){return i++?{done:true}:{value:1,done:false}}, \
+           return:function(){closed++;return {}}}}}; \
+         var old=Set.prototype.add;Set.prototype.add=function(){throw 8};var caught; \
+         try{new Set(it)}catch(e){caught=e}Set.prototype.add=old;caught+':'+closed",
+    );
+    agrees(
+        "var p=Object.getPrototypeOf([][Symbol.iterator]()),old=p.next,calls=0; \
+         p.next=function(){calls++;return old.call(this)};var s=new Set([1,2]); \
+         p.next=old;calls+':'+s.size",
+    );
+    agrees(
+        "var log=[],entry=[];Object.defineProperty(entry,'0',{get:function(){log.push('k');return 'a'}}); \
+         Object.defineProperty(entry,'1',{get:function(){log.push('v');return 3}}); \
+         var m=new Map([entry]);log.join(',')+':'+m.get('a')",
+    );
+    agrees(
+        "var old=Set.prototype.add,ok=false;Set.prototype.add=Map.prototype.set; \
+         try{new Set([1])}catch(e){ok=e instanceof TypeError}Set.prototype.add=old;ok",
+    );
+    agrees(
+        "var a=[1,2],old=Set.prototype.add;Object.defineProperty(Set.prototype,'add',{ \
+         configurable:true,get:function(){delete a[1];return old}});var s=new Set(a); \
+         delete Set.prototype.add;Set.prototype.add=old;s.size+':'+s.has(undefined)",
+    );
+}
+
+#[test]
 fn iterable_constructor_observes_overridden_adder() {
     agrees(
         "var calls = 0; var old = Map.prototype.set; \
