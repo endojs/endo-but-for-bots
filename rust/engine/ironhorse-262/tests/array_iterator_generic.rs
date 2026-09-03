5b72b182dad88028a259fbd926df06ddff7ca4ae
kumavis <aaron@kumavis.me>
2026-09-03 17:23:04 +0900

    feat(ironhorse): iterate generic array-like receivers

---
+++ rust/engine/ironhorse-262/tests/array_iterator_generic.rs
@@ -0,0 +1,53 @@
+//! Oracle-backed coverage for generic Array iterator receivers.
+
+use ironhorse_262::{dual_run, Agreement};
+
+fn agrees(source: &str) {
+    let run = dual_run(source).expect("the pinned XS oracle must start");
+    assert_eq!(
+        run.agreement,
+        Agreement::BothComplete,
+        "`{source}` must complete on both engines: {run:?}",
+    );
+    assert!(
+        run.result_agrees,
+        "`{source}` diverged: oracle={:?} ironhorse={:?}",
+        run.oracle_result, run.ironhorse_result,
+    );
+}
+
+#[test]
+fn ordinary_array_like_receivers_yield_values_keys_and_entries() {
+    for source in [
+        "var i=Array.prototype.values.call({length:2,0:'a'});var a=i.next(),b=i.next(),c=i.next();a.value+':'+b.value+':'+c.done",
+        "var i=Array.prototype.keys.call({length:2});i.next().value+':'+i.next().value+':'+i.next().done",
+        "var i=Array.prototype.entries.call({length:1,0:'x'});var p=i.next().value;p[0]+':'+p[1]",
+        "var i=Array.prototype.values.call('ab');i.next().value+i.next().value+':'+i.next().done",
+        "Array.prototype.values.call(3).next().done",
+    ] {
+        agrees(source);
+    }
+}
+
+#[test]
+fn iteration_rechecks_live_length_and_properties() {
+    for source in [
+        "var o={length:2,0:'a',1:'b'},i=Array.prototype.values.call(o);var a=i.next().value;o.length=1;a+':'+i.next().done",
+        "var o={length:2,0:'a',1:'b'},i=Array.prototype.values.call(o);var a=i.next().value;o[1]='z';a+i.next().value",
+        "var log=[],p=new Proxy({length:1,0:'x'},{get:function(t,k,r){log.push(String(k));return Reflect.get(t,k,r)}}),i=Array.prototype.values.call(p);i.next().value+':'+log.join(',')",
+        "var marker={},o={length:1};Object.defineProperty(o,'0',{get:function(){throw marker}});var i=Array.prototype.values.call(o);try{i.next();false}catch(e){e===marker}",
+    ] {
+        agrees(source);
+    }
+}
+
+#[test]
+fn nullish_receivers_throw_and_revoked_proxies_fail_on_advance() {
+    for source in [
+        "try{Array.prototype.values.call(null);false}catch(e){e instanceof TypeError}",
+        "try{Array.prototype.keys.call(undefined);false}catch(e){e instanceof TypeError}",
+        "var r=Proxy.revocable({length:0},{}),i=Array.prototype.values.call(r.proxy);r.revoke();try{i.next();false}catch(e){e instanceof TypeError}",
+    ] {
+        agrees(source);
+    }
+}
